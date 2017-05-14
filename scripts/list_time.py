6c0fce2618f8e8386745147ee1749c661b268ed3
tanabee <tanabee1986@gmail.com>
2017-05-14 21:04:28 +0900

    スクリプト追加

---
+++ scripts/list_time.py
@@ -0,0 +1,9 @@
+import timeit
+
+normal_py_sec = timeit.timeit('sum(x*x for x in range(1000))', number=10000)
+naive_np_sec  = timeit.timeit('sum(na*na)', setup="import numpy as np; na=np.arange(1000)", number=10000)
+good_np_sec   = timeit.timeit('na.dot(na)', setup="import numpy as np; na=np.arange(1000)", number=10000)
+
+print("Normal Python: %f sec" %normal_py_sec)
+print("Naive Numpy: %f sec" %naive_np_sec)
+print("Good Numpy: %f sec" %good_np_sec)
